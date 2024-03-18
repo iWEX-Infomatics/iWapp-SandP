222290abe65051c6fd1da2a9bc6ba8105754e127
arun.akds@gmail.com <arun.akds@gmail.com>
2024-03-18 22:22:01 +0530

    fix:Custom Default Values - Display Item Tax Item onchange of Item Tax Percentage

--- iwapp_sandp/iwapp_sandp/doctype/custom_default_values/custom_default_values.js
+++ iwapp_sandp/iwapp_sandp/doctype/custom_default_values/custom_default_values.js
@@ -2,26 +2,61 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Custom Default Values', {
-	refresh: function (frm) {
+    refresh: function (frm) {
         frm.fields_dict['default_values'].grid.get_field('customer_account').get_query = function (doc, cdt, cdn) {
             var child = locals[cdt][cdn];
             return {
                 filters: {
                     'company': child.company,
-					'account_currency':child.currency,
-					'name':"Debtors" + " - " + child.currency + " - " + child.abbreviation
+                    'account_currency': child.currency,
+                    'name': "Debtors" + " " + child.currency + "- " + child.abbreviation
                 }
             };
         };
-		frm.fields_dict['default_values'].grid.get_field('supplier_account').get_query = function (doc, cdt, cdn) {
+        frm.fields_dict['default_values'].grid.get_field('supplier_account').get_query = function (doc, cdt, cdn) {
             var child = locals[cdt][cdn];
             return {
                 filters: {
                     'company': child.company,
-					'account_currency':child.currency,
-					'name':"Creditors" + " - " + child.currency + " - " + child.abbreviation
+                    'account_currency': child.currency,
+                    'name': "Creditors" + " " + child.currency + " - " + child.abbreviation
                 }
             };
         };
+    },
+    custom_item_tax_percentage:function(frm){
+        frm.clear_table("taxes")
+        frm.refresh_field("taxes")
+        if(frm.doc.custom_item_tax_percentage){
+            frappe.call({
+                method:"iwapp_sandp.events.item.get_tax_template",
+                args:{
+                    tax:frm.doc.custom_item_tax_percentage
+                },
+                callback:function(r){
+                    if(r.message){
+                        $.each(r.message, function(idx, tax){
+                            let child = frm.add_child("taxes");
+                            child.item_tax_template = tax.name
+                            child.tax_category = tax.category
+                            frm.refresh_fields("taxes");
+                        })
+                    }
+                }
+            })
+        }
+    }
+});
+
+frappe.ui.form.on('Custom Default Values Child', {
+    tax_category: function (frm, cdt, cdn) {
+        let row = locals[cdt][cdn];
+        if (row.tax_category) {
+            const category_list = ["Overseas", "Export"]; // Make category_list a constant
+            if (category_list.includes(row.tax_category)) {
+                // Corrected set_value function call
+                frappe.model.set_value(cdt, cdn, "country", "");
+            }
+        }
     }
 });
--- iwapp_sandp/iwapp_sandp/doctype/custom_default_values/custom_default_values.py
+++ iwapp_sandp/iwapp_sandp/doctype/custom_default_values/custom_default_values.py
@@ -6,3 +6,8 @@ from frappe.model.document import Document
 
 class CustomDefaultValues(Document):
 	pass
+	def validate(self):
+		if self.default_values:
+			for i in self.default_values:
+				if i.tax_category in ["Overseas", "Export"]:
+					i.country = ""
